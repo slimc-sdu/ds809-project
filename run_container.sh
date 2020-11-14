61c3eb256f97313b7385045a7fe7ee9e5fe9f8c1
Christian <christian@skjerning.eu>
2020-11-14 17:09:08 +0100

    changed to jupyter notebook

---
+++ run_container.sh
@@ -0,0 +1,2 @@
+docker run --runtime=nvidia -v $PWD:/tf/notebooks/dm809-project -p 8888:8888 -p 6006:6006 tensorflow/tensorflow:latest-gpu-jupyter
+#docker exec f1d4ed017a98 pip install keras
--- run_training_container.sh
+++
@@ -1,6 +0,0 @@
-
-
-# https://www.tensorflow.org/install/docker
-docker run --runtime=nvidia -it -v $PWD:/tmp -w /tmp/src tensorflow/tensorflow:latest-gpu bash
-
-#"pip install -r requirements.txt && python ./train.py"
