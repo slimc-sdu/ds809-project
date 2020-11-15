5f9e2ffa3386e5236d1e5bdfc1df86e7cfc49801
Christian <christian@skjerning.eu>
2020-11-15 12:48:52 +0100

    update on classifier

--- run_container.sh
+++ run_container.sh
@@ -1,1 +1,1 @@
-docker run -it --runtime=nvidia -v $PWD:/tf/notebooks/dm809-project -p 8888:8888 tensorflow/tensorflow:latest-gpu-jupyter
+docker run -it --runtime=nvidia --net=host -v $PWD:/tf/notebooks/dm809-project -p 8888:8888 -p  6006-6015:6006-6015 tensorflow/tensorflow:latest-gpu-jupyter
